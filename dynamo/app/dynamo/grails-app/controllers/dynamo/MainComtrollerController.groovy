7b720cd7ca0dc3c25bfdfb7b434e8cd341fa88d1
Miro <t.miro99@gmail.com>
2016-03-22 21:21:27 +0100

    presun registrácie do bootstrapu

--- dynamo/app/dynamo/grails-app/controllers/dynamo/MainComtrollerController.groovy
+++ dynamo/app/dynamo/grails-app/controllers/dynamo/MainComtrollerController.groovy
@@ -2,12 +2,6 @@ package dynamo
 
 import grails.converters.JSON
 import org.apache.curator.ensemble.EnsembleProvider
-import org.apache.curator.framework.CuratorFramework
-import org.apache.curator.framework.CuratorFrameworkFactory
-import org.apache.curator.retry.RetryOneTime
-import org.apache.curator.x.discovery.ServiceDiscoveryBuilder
-import org.apache.curator.x.discovery.ServiceInstance
-import org.apache.curator.x.discovery.UriSpec
 import org.grails.web.json.JSONObject
 
 class MainController implements EnsembleProvider {
@@ -17,36 +11,6 @@ class MainController implements EnsembleProvider {
 
         JSONObject obj = new JSONObject();
         log.info("Index");
-
-        CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient("zookeeper.dev:2181", new RetryOneTime(1000))
-        curatorFramework.start()
-        ServiceInstance<Void> serviceInstance = ServiceInstance.builder()
-                .uriSpec(new UriSpec("{scheme}://{address}:{port}"))
-                .address('localhost')
-                .port(8080)
-                .name("dynamo")
-                .build()
-        ServiceDiscoveryBuilder.builder(Void)
-                .basePath("/traefik")
-                .client(curatorFramework)
-                .thisInstance(serviceInstance)
-                .build()
-                .start()
-
-        /*
-        CuratorFramework client = CuratorFrameworkFactory.newClient("zookeeper.dev:2181", new RetryOneTime(1000))
-        client.start();
-        try {
-            client.blockUntilConnected();
-            log.info("connected");
-            client.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).withACL(ZooDefs.Ids.OPEN_ACL_UNSAFE).forPath("/ABC", "ggg".getBytes());
-            obj.put("status", "200");
-            obj.put("statusMessage", "success");
-        } catch (Exception e) {
-            obj.put("status", "400");
-            obj.put("statusMessage", e.getMessage());
-            log.error("Error while registering server: "+e.getMessage());
-        }*/
 
         count++;
 
@@ -68,5 +32,6 @@ class MainController implements EnsembleProvider {
     void close() throws IOException {
         log.info("CLOSE");
     }
+
 
 }
--- dynamo/app/dynamo/grails-app/init/BootStrap.groovy
+++ dynamo/app/dynamo/grails-app/init/BootStrap.groovy
@@ -1,7 +1,36 @@
+import org.apache.curator.framework.CuratorFramework
+import org.apache.curator.framework.CuratorFrameworkFactory
+import org.apache.curator.retry.RetryOneTime
+import org.apache.curator.x.discovery.ServiceDiscoveryBuilder
+import org.apache.curator.x.discovery.ServiceInstance
+import org.apache.curator.x.discovery.UriSpec
+
 class BootStrap {
 
+    private static final String basePath = "/traefik";
+
     def init = { servletContext ->
+
+        registerInZookeeper();
     }
     def destroy = {
+    }
+
+
+    private static void registerInZookeeper() {
+        CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient("zookeeper.dev:2181", new RetryOneTime(1000))
+        curatorFramework.start()
+        ServiceInstance<Void> serviceInstance = ServiceInstance.builder()
+                .uriSpec(new UriSpec("{scheme}://{address}:{port}"))
+                .address('localhost')
+                .port(8080)
+                .name("dynamo")
+                .build()
+        ServiceDiscoveryBuilder.builder(Void)
+                .basePath(basePath)
+                .client(curatorFramework)
+                .thisInstance(serviceInstance)
+                .build()
+                .start()
     }
 }
