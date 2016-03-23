a41b8171ad749f38f65b17b89a08b09e852db607
Miro <t.miro99@gmail.com>
2016-03-23 19:50:00 +0100

    test traefiku

--- dynamo/app/dynamo/grails-app/controllers/dynamo/MainComtrollerController.groovy
+++ dynamo/app/dynamo/grails-app/controllers/dynamo/MainComtrollerController.groovy
@@ -2,6 +2,12 @@ package dynamo
 
 import grails.converters.JSON
 import org.apache.curator.ensemble.EnsembleProvider
+import org.apache.curator.framework.CuratorFramework
+import org.apache.curator.framework.CuratorFrameworkFactory
+import org.apache.curator.retry.RetryOneTime
+import org.apache.curator.x.discovery.ServiceDiscoveryBuilder
+import org.apache.curator.x.discovery.ServiceInstance
+import org.apache.curator.x.discovery.UriSpec
 import org.grails.web.json.JSONObject
 
 class MainController implements EnsembleProvider {
@@ -11,8 +17,24 @@ class MainController implements EnsembleProvider {
 
         JSONObject obj = new JSONObject();
         log.info("Index");
-
         count++;
+
+        CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient("zookeeper.dev:2181", new RetryOneTime(1000))
+        curatorFramework.start()
+        ServiceInstance<Void> serviceInstance = ServiceInstance.builder()
+                .uriSpec(new UriSpec("{scheme}://{address}:{port}"))
+                .address('localhost')
+                .port(8080)
+                .name("dynamo")
+                .build()
+        ServiceDiscoveryBuilder.builder(Void)
+                .basePath("/traefik/backends/backend1/servers")
+                .client(curatorFramework)
+                .thisInstance(serviceInstance)
+                .build()
+                .start()
+
+
 
         obj.put("count", Integer.toString(count));
         render obj as JSON
@@ -32,6 +54,4 @@ class MainController implements EnsembleProvider {
     void close() throws IOException {
         log.info("CLOSE");
     }
-
-
 }
